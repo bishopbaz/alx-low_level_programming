50aaab97f4eefb8cff095e66ca43264333203769
bishopbaz <basseygbassey@gmail.com>
2022-09-08 18:29:39 -0700

    4. Hello, puts
    mandatory
    
    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 /**
  *
  * main - Entry point
+ *
+ * Description: Using the main funcion
+ * This program prints "Programming is like building a multilingual puzzle
  * 
  * Return: Always 0 (success)
  */
