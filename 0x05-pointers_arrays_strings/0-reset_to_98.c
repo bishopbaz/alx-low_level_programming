edad9efcd35522e8363538c39f42fef6c8a09785
bishopbaz <basseygbassey@gmail.com>
2022-09-20 11:23:52 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,15 @@
 #include "main.h"
 
-
-
 /**
+ * reset_to_98 - function that takes a pointer to an integer parameter
  *
- *  * reset_to_98 - function that takes a pointer to an integer parameter
+ * @n: integer pointer
  *
- *   * @n: integer pointer
+ * Return: 0 Success
  *
- *    * Return: 0 Success
- *
- *     */
-
-
+ */
 
 void reset_to_98(int *n)
-
 {
 
 		*n = 98;
